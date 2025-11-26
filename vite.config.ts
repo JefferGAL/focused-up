2d6c2f68ac6217053408a3a1c118f0aeef0fae66
JefferGAL <jefferg@gmail.com>
2025-11-26 10:01:07 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/focus-up/', // REQUIRED: Tells the app it lives at https://jeffergal.github.io/focus-up/
+  base: '/focused-up/', // REQUIRED: Tells the app it lives at https://jeffergal.github.io/focused-up/
   build: {
     outDir: 'docs', // Output the built site to 'docs' folder so GitHub can read it
   }
